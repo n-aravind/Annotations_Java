4559ff424f74e11806b98381ee08ce4172e73e30
Aravind <aravindnareddy@gmail.com>
2019-02-24 14:33:06 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Vehicle myVehicle = new Vehicle("Civic");
+        System.out.println(myVehicle.toString());
+    }
+}
